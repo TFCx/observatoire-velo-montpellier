9e1b593a3c3e6796fcaeadc065103949981dd70c
Jean-David Génevaux <genevaux@circle-dental.com>
2024-08-04 18:39:36 +0200

    Fix padding on bubbleName

--- components/home/LinesSection.vue
+++ components/home/LinesSection.vue
@@ -3,7 +3,7 @@
     <div v-for="voie in voies" :key="voie.line" class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-velocite-yellow-5">
       <div class="flex-shrink-0">
         <div
-          class="h-10 w-10 rounded-full flex items-center justify-center text-white font-bold"
+          class="px-3 py-1 rounded-full flex items-center justify-center text-white font-bold"
           :style="`background-color: ${getLineColor(voie.line)}`"
         >
           {{ voie.lineNameShort }}
