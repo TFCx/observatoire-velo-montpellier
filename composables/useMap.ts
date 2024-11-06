00ed4e42b8007b6b4acc9d69ec364f2c3340df18
RoueLibre73 <45666255+RoueLibre73@users.noreply.github.com>
2024-11-06 22:30:00 +0100

    test couleur ligne done et wip Plotdonesections

--- composables/useMap.ts
+++ composables/useMap.ts
@@ -184,7 +184,7 @@ export const useMap = () => {
       source: 'done-sections',
       paint: {
         'line-width': 4,
-        'line-color': ['get', 'color']
+        'line-color': '#0c2d42'
       }
     });
   }
@@ -205,7 +205,7 @@ export const useMap = () => {
       source: 'wip-sections',
       paint: {
         'line-width': 4,
-        'line-color': ['get', 'color'],
+        'line-color': '#0c2d42'
         'line-dasharray': [0, 2, 2]
       }
     });
