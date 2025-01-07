3c6d66318572e492cad7c3507a9a100d3991affc
RoueLibre73 <45666255+RoueLibre73@users.noreply.github.com>
2025-01-07 21:39:38 +0100

    ajouter évolution sur page accueil (copie marseille)

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -55,6 +55,12 @@
                 <NuxtLink to="/carte-interactive" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-lvv-blue-600 hover:shadow-lg sm:px-8 transition duration-300 transform hover:scale-105">
                   Carte interactive
                 </NuxtLink>
+                <NuxtLink
+                  to="/evolution"
+                  class="flex items-center justify-center px-4 py-3 text-base font-medium rounded-md text-cvv-blue-600 border border-cvv-blue-600 sm:px-8 duration-100 hover:bg-cvv-blue-600 hover:text-white"
+                >
+                  Évolution
+                </NuxtLink>
                 <NuxtLink to="/plan-officiel" class="flex items-center justify-center px-4 py-3 text-base font-medium rounded-md shadow-sm text-lvv-blue-600 border border-lvv-blue-600 hover:shadow-lg sm:px-8 transition duration-300 transform hover:scale-105">
                   Plan officiel
                 </NuxtLink>
