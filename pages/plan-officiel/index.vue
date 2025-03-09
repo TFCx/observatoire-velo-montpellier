1327fff4650538f0903728128bb4b27fcf817a88
feurer <denis.feurer@yahoo.fr>
2025-03-09 15:11:32 +0100

    Fix lien mort plan officiel vélolignes

--- pages/plan-officiel/index.vue
+++ pages/plan-officiel/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full">
     <img
-      src="https://www.montpellier.fr/uploads/Image/4b/43449_216_20220531_REV-version-definitive.jpg"
+      src="https://web.archive.org/web/20220804235824im_/https://www.montpellier3m.fr/sites/default/files/2021/20220531_rev_version_definitive.jpg"
       class="object-contain w-full h-screen"
       :alt="`plan officiel des ${getRevName()}`"
     >
